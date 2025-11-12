bdc1bb1303325ae426e886dec86ace80f02897ae
rritajj <ritag.rezk@student.giu-uni.de>
2025-11-12 15:18:18 +0200

    Define Reptile class inheriting from Animal

--- Reptile.h
+++ Reptile.h
@@ -3,14 +3,14 @@
 
 #include "Animal.h"
 
-//define reptile class here
-
-
-
-
-
-
-
-
+class Reptile : public Animal {
+private:
+    bool isVenomous;
+public:
+    Reptile( std::string& name, int age, bool isHungry, bool isVenomous);
+    ~Reptile() override;
+    void display() override;
+    bool getIsVenomous() ;
+};
 
 #endif
